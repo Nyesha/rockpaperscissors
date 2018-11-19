53a36ddc519bd68399f1e7c6f53807c0b47c6374
Nyesha Jones <neeshajolmfao@gmail.com>
2018-11-19 23:16:01 +0000

    Unit 2 Sessions work

--- js/script.js
+++ js/script.js
@@ -4,8 +4,22 @@
 
 //GLOBAL VARIABLES
 /* global $ */
-
+ var userChoice = ""
+ var computerChoice = ""
+ var winner = ""
+ var randomNumber =["Rock","rock","Paper","paper","Scissors","scissors"]
 
 
 // DOCUMENT READY FUNCTION BELOW
+var aRandomNumber = 0
+$("#shoot").click(function(){
+    userChoice=$("#input").val()
+    $("#userChoice").text(userChoice)
+   
+})
 
+$("#shoot").click(function(){
+    computerChoice= $("randomNumber").val()
+    $(computerChoice).text(computerChoice)
+    
+})
