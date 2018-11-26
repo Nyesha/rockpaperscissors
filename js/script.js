946858632accb1f39834b168c217efdcecad75ec
Nyesha Jones <neeshajolmfao@gmail.com>
2018-11-26 23:02:20 +0000

    finally some progress

--- js/script.js
+++ js/script.js
@@ -7,19 +7,29 @@
  var userChoice = ""
  var computerChoice = ""
  var winner = ""
- var randomNumber =["Rock","rock","Paper","paper","Scissors","scissors"]
+ var randomNumber = Math.random();
 
 
 // DOCUMENT READY FUNCTION BELOW
-var aRandomNumber = 0
 $("#shoot").click(function(){
     userChoice=$("#input").val()
     $("#userChoice").text(userChoice)
+    console.log(userChoice);
    
 })
 
 $("#shoot").click(function(){
     computerChoice= $("randomNumber").val()
-    $(computerChoice).text(computerChoice)
+    console.log(computerChoice)
+    
+if (randomNumber > .60) {
+    computerChoice = "Rock"
+} else if (randomNumber < .70 || .60) {
+    computerChoice = "Paper" 
+} else {
+    computerChoice = "Scissors"
+} 
+
+$("#computerChoice").html(computerChoice)
     
 })
